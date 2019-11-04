28cf9392b56d1826e9e600de248292c527d1cc81
Takis Zourntos <takis.zourntos@emads.org>
2019-11-04 14:42:57 -0500

    did some filter design for audio application

--- MATLAB/Examples/prog1.m
+++ MATLAB/Examples/prog1.m
@@ -25,23 +25,25 @@ ym = ym';
 %% 
 %  manipulate data
 %
-yf2 = rand(max(size(yf)))*yf;
-ym2 = rand(max(size(ym)))*ym;
-% Arf = eye(max(size(yf)));
-% Mrf = flip(Arf);
-% yf2 = Mrf*yf;
-% 
-% Arm = eye(max(size(ym)));
-% Mrm = flip(Arm);
-% ym2 = Mrm*ym;
-% ym2 = interp(ym2, 4);
-% ym2 = ym2';
+% yf2 = rand(max(size(yf)))*yf;
+% ym2 = rand(max(size(ym)))*ym;
+Arf = eye(max(size(yf)));
+Mrf = flip(Arf);
+yf2 = Mrf*yf;
+
+Arm = eye(max(size(ym)));
+Mrm = flip(Arm);
+ym2 = Mrm*ym;
+ym2 = interp(ym2, 4);
+ym2 = ym2';
 
 %%
 %  write audio files
 %
-ffile2 = 'human_voice_female_changed.mp4';
-mfile2 = 'human_voice_male_changed.mp4';
+ffile2 = 'human_voice_female_changed.ogg';
+mfile2 = 'human_voice_male_changed.ogg';
+!rm human_voice_female_changed.ogg
+!rm human_voice_male_changed.ogg
 audiowrite(ffile2,yf2,FSf);
 audiowrite(mfile2,ym2,FSm);
 
